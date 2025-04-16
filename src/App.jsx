4d7f59f496558bfd54f8f6a521485e312b3cde89
Kubagus <ahmadkubagus230@gmail.com>
2025-04-16 12:51:29 +0700

    nambahin judul

--- src/App.jsx
+++ src/App.jsx
@@ -101,6 +101,7 @@ const App = () => {
 								<li key={movie.$id}>
 									<p>{index + 1}</p>
 									<img src={movie.poster_url} alt={movie.title} />
+									<p>{ movie.title}</p>
 								</li>
 							))}
 						</ul>
